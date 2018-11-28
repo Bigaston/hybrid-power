13ca13a66b629a91a4b27c605b57c70a49dc32cc
Bigaston <bigaston1@gmail.com>
2018-11-28 19:02:38 +0100

    Add files via upload

---
+++ Other Files/Fond.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Fond" tilewidth="80" tileheight="80" tilecount="13" columns="13">
+ <image source="../img/tiles.png" width="1040" height="80"/>
+</tileset>
---
+++ Other Files/assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="assets" tilewidth="80" tileheight="80" tilecount="2" columns="2">
+ <image source="../img/assets.png" width="160" height="80"/>
+</tileset>
